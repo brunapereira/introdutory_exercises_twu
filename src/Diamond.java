7c87a7282480b7334f771f5124cc9968d7bd29c7
Bruna <ppbruna2@gmail.com>
2015-09-10 18:12:23 -0300

    finished 'Diamond' exercise

---
+++ src/Diamond.java
@@ -0,0 +1,28 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+/**
+ * Created by bpereira on 9/10/15.
+ */
+public class Diamond {
+
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = Integer.parseInt(s.nextLine());
+        int repeat = 1;
+        for (int i = number; i > 0; i--) {
+            System.out.print(StringUtils.repeat(" ", i - 1));
+            System.out.println(StringUtils.repeat("*", repeat));
+            if (i != 1) repeat += 2;
+        }
+
+
+        for (int i = 0; i < number; i++) {
+            repeat -= 2;
+            System.out.print(StringUtils.repeat(" ", i + 1));
+            System.out.println(StringUtils.repeat("*", repeat));
+        }
+    }
+}
